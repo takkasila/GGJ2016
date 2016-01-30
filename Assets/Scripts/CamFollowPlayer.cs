fac5cf16ecaf90e79e09cc26f7e3dd531c5a96c5
Takkasila Saichol <takkasila@outlook.com>
2016-01-30 15:39:05 +0700

    Init commit

---
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollowPlayer : MonoBehaviour {
+
+    public Transform Player;
+    public Transform sceneBoundLeft, sceneBoundRight;
+    public Transform testBox;
+    float camBlockWidth;
+
+    void Start()
+    {
+        calCamBlockWidth_fromPerspective();
+        Debug.Log(camBlockWidth);
+        testBox.position = new Vector3(camBlockWidth / 2, testBox.position.y, testBox.position.z);
+        
+    }
+
+    void calCamBlockWidth_fromPerspective()
+    {
+        Vector3 p = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, -transform.position.z));
+        camBlockWidth = Mathf.Abs(p.x * 2);
+    }
+	void Update () {
+        // Will change to LERP
+        transform.position = new Vector3(Player.transform.position.x, transform.position.y, transform.position.z);
+
+        if(transform.position.x - camBlockWidth/2 < sceneBoundLeft.position.x)
+        {
+            transform.position = new Vector3(sceneBoundLeft.position.x + camBlockWidth / 2, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x + camBlockWidth/2 > sceneBoundRight.position.x)
+        {
+            transform.position = new Vector3(sceneBoundRight.position.x - camBlockWidth / 2, transform.position.y, transform.position.z);
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float walkSpeed = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Time.deltaTime * walkSpeed;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right* Time.deltaTime * walkSpeed;
+        }
+	}
+}
