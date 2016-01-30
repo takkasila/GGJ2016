4037baa020cead85039da5e010ebeb3056a82add
Takkasila Saichol <takkasila@outlook.com>
2016-01-30 16:58:51 +0700

    Key sequences listener

---
+++ Assets/Scripts/KeySequence.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeySequence : MonoBehaviour {
+
+    public string keySequences;
+    public GameObject listenerObject;
+
+    int waitKeyIndex  = 0;
+	
+	void Update () {
+        if(Input.anyKeyDown)
+        {
+            if (string.IsNullOrEmpty(Input.inputString))
+                return;
+            
+            char key = Input.inputString[0];
+            if (key == keySequences[waitKeyIndex])
+            {
+                waitKeyIndex++;
+            }
+            else
+            {
+                waitKeyIndex = 0;
+            }
+
+        }
+
+        if(waitKeyIndex == keySequences.Length)
+        {
+            listenerObject.SendMessage("FinishedKeySequences");
+            this.enabled = false;
+        }
+	}
+}
