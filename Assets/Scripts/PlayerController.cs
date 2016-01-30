41ce3462c3c0a58df19488f4c6cb18d5645b1fe9
Takkasila Saichol <takkasila@outlook.com>
2016-01-31 05:04:05 +0700

    Animated player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,54 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float walkSpeed = 1;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+    public Animator anim;
+
 	void Update () {
+
+        resetAnimBool();
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            anim.SetBool("z", true);
+            return;
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            anim.SetBool("x", true);
+            return;
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            anim.SetBool("c", true);
+            return;
+        }
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            anim.SetBool("v", true);
+            return;
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * Time.deltaTime * walkSpeed;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            anim.SetBool("Walking", true);
         }
         else if(Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right* Time.deltaTime * walkSpeed;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            anim.SetBool("Walking", true);
         }
+        
+
 	}
+
+    void resetAnimBool()
+    {
+        anim.SetBool("Walking", false);
+        anim.SetBool("z", false);
+        anim.SetBool("x", false);
+        anim.SetBool("c", false);
+        anim.SetBool("v", false);
+    }
 }
