d52214bf3c175e9af745218684bc31f4a8b4808e
Takkasila Saichol <takkasila@outlook.com>
2016-01-31 09:29:21 +0700

    Import mmm models

---
+++ Assets/Scripts/WarpToScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpToScene : MonoBehaviour {
+
+    public string sceneName;
+    public float delay = 2;
+    public Animator eyeLidAnim;
+	void OnTriggerEnter(Collider someObj)
+    {
+        if (someObj.gameObject.tag == "Player")
+        {
+            eyeLidAnim.SetBool("CloseEyeLid", true);
+            StartCoroutine(wait());
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(delay);
+        Application.LoadLevel(sceneName);
+    }
+}
