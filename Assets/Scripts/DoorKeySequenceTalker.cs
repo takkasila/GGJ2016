62f9473f0df46a5f0dd05f85b79e8c6bcb933545
Takkasila Saichol <takkasila@outlook.com>
2016-01-31 10:25:07 +0700

    Finished scene1 sequences

---
+++ Assets/Scripts/DoorKeySequenceTalker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorKeySequenceTalker : MonoBehaviour {
+
+    public GameObject mainGate;
+	void FinishedKeySequences()
+    {
+        mainGate.SendMessage("Open");
+    }
+}
--- Assets/Scripts/WarpToScene.cs
+++ Assets/Scripts/WarpToScene.cs
@@ -4,19 +4,27 @@ using System.Collections;
 public class WarpToScene : MonoBehaviour {
 
     public string sceneName;
+    public float delayBeforeCloseEye = 0;
     public float delay = 2;
     public Animator eyeLidAnim;
+    public bool changeWhenTrigger = false;
 	void OnTriggerEnter(Collider someObj)
     {
-        if (someObj.gameObject.tag == "Player")
+        if (someObj.gameObject.tag == "Player" && changeWhenTrigger)
         {
             eyeLidAnim.SetBool("CloseEyeLid", true);
             StartCoroutine(wait());
         }
     }
 
+    void FinishedKeySequences()
+    {
+        StartCoroutine(wait());
+    }
     IEnumerator wait()
     {
+        yield return new WaitForSeconds(delayBeforeCloseEye);
+        eyeLidAnim.SetBool("CloseEyeLid", true);
         yield return new WaitForSeconds(delay);
         Application.LoadLevel(sceneName);
     }
